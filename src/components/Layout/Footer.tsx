ff4ee7f196fa6378625a13f289ccb159d235fe6f
Matteo Decorsaire <mattdecorsaire@gmail.com>
2024-06-22 21:20:44 -0400

    lint

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,91 +1,91 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Center, Flex, Group, Text} from '@mantine/core';
-import {useLocation, useNavigate} from 'react-router-dom';
-import {Route, routes} from '@/Router';
-import {useNavigationStore} from "@/store/Navigation.store";
+import React, { useEffect, useState } from 'react';
+import { Button, Center, Flex, Group, Text } from '@mantine/core';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Route, routes } from '@/Router';
+import { useNavigationStore } from '@/store/Navigation.store';
 
 interface FooterButtonProps extends Route {
-    onClick: () => void;
-    keyRoute: string;
-    indexRoute: number;
-    setActiveRoute: (route: string) => void;
-    activeRoute: string;
+  onClick: () => void;
+  keyRoute: string;
+  indexRoute: number;
+  setActiveRoute: (route: string) => void;
+  activeRoute: string;
 }
 
 function FooterButton({
-                          icon,
-                          path,
-                          name,
-                          keyRoute,
-                          setActiveRoute,
-                          activeRoute,
-                      }: FooterButtonProps) {
-    const navigate = useNavigate();
-    const store = useNavigationStore();
+  icon,
+  path,
+  name,
+  keyRoute,
+  setActiveRoute,
+  activeRoute,
+}: FooterButtonProps) {
+  const navigate = useNavigate();
+  const store = useNavigationStore();
 
-    const onClick = (destRoute: string) => {
-        store.navigate(navigate, window.location.pathname, destRoute);
-        setActiveRoute(destRoute);
-    };
+  const onClick = (destRoute: string) => {
+    store.navigate(navigate, window.location.pathname, destRoute);
+    setActiveRoute(destRoute);
+  };
 
-    return (
-        <Center>
-            <Button
-                variant="subtle"
-                m={0}
-                px={5}
-                h={60}
-                w={100}
-                onClick={() => onClick(routes[keyRoute].path)}
-                radius={200}
-            >
-                <Flex justify="center" direction="column" align="center">
-                    {icon}
-                    <Text fz={12} fw={routes[keyRoute].path === activeRoute ? 800 : undefined}>
-                        {name}
-                    </Text>
-                </Flex>
-            </Button>
-        </Center>
-    );
+  return (
+    <Center>
+      <Button
+        variant="subtle"
+        m={0}
+        px={5}
+        h={60}
+        w={100}
+        onClick={() => onClick(routes[keyRoute].path)}
+        radius={200}
+      >
+        <Flex justify="center" direction="column" align="center">
+          {icon}
+          <Text fz={12} fw={routes[keyRoute].path === activeRoute ? 800 : undefined}>
+            {name}
+          </Text>
+        </Flex>
+      </Button>
+    </Center>
+  );
 }
 
 export default function Footer() {
-    const [activeRoute, setActiveRoute] = useState<string>('');
-    const location = useLocation();
+  const [activeRoute, setActiveRoute] = useState<string>('');
+  const location = useLocation();
 
-    useEffect(() => {
-        setActiveRoute(location.pathname);
-    }, [location.pathname]);
+  useEffect(() => {
+    setActiveRoute(location.pathname);
+  }, [location.pathname]);
 
-    return (
-        <Group
-            grow
-            justify="center"
-            align="stretch"
-            h={80}
-            style={{
-                position: 'fixed',
-                bottom: 0,
-                left: 0,
-                right: 0,
-                borderTop: '0.05em solid #d1cfcf'
-            }}
-            bg="white"
-        >
-            {Object.keys(routes).map((key, index) => (
-                <FooterButton
-                    onClick={function (): void {
-                        throw new Error('Function not implemented.');
-                    }}
-                    setActiveRoute={setActiveRoute}
-                    activeRoute={activeRoute}
-                    keyRoute={key}
-                    indexRoute={index}
-                    key={index}
-                    {...routes[key]}
-                />
-            ))}
-        </Group>
-    );
+  return (
+    <Group
+      grow
+      justify="center"
+      align="stretch"
+      h={80}
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        borderTop: '0.05em solid #d1cfcf',
+      }}
+      bg="white"
+    >
+      {Object.keys(routes).map((key, index) => (
+        <FooterButton
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+          setActiveRoute={setActiveRoute}
+          activeRoute={activeRoute}
+          keyRoute={key}
+          indexRoute={index}
+          key={index}
+          {...routes[key]}
+        />
+      ))}
+    </Group>
+  );
 }
--- src/store/Navigation.store.tsx
+++ src/store/Navigation.store.tsx
@@ -1,47 +1,47 @@
-import {createContext, useContext} from 'react';
-import {makeAutoObservable} from 'mobx';
-import {NavigateFunction} from 'react-router-dom';
-import {routes} from '@/Router';
+import { createContext, useContext } from 'react';
+import { makeAutoObservable } from 'mobx';
+import { NavigateFunction } from 'react-router-dom';
+import { routes } from '@/Router';
 
 class NavigationStore {
-    routePosition: 'left' | 'right' = 'left';
+  routePosition: 'left' | 'right' = 'left';
 
-    constructor() {
-        makeAutoObservable(this);
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  navigate(navigate: NavigateFunction, sourcePath: string, destinationPath: string) {
+    const sourceRoute = Object.entries(routes).find(([, route]) => route.path === sourcePath);
+    const destinationRoute = Object.entries(routes).find(
+      ([, route]) => route.path === destinationPath
+    );
+
+    if (!destinationRoute) {
+      throw new Error(`Invalid route path: ${sourcePath} to ${destinationPath}`);
     }
-
-    navigate(navigate: NavigateFunction, sourcePath: string, destinationPath: string) {
-        const sourceRoute = Object.entries(routes).find(([, route]) => route.path === sourcePath);
-        const destinationRoute = Object.entries(routes).find(
-            ([, route]) => route.path === destinationPath
-        );
-
-        if (!destinationRoute) {
-            throw new Error(`Invalid route path: ${sourcePath} to ${destinationPath}`);
-        }
-        if (!sourceRoute) {
-            navigate(destinationPath);
-            return;
-        }
-        const sourceIndex = this.getRoutePosition(sourceRoute[0]);
-        const destinationIndex = this.getRoutePosition(destinationRoute[0]);
-
-        if (sourceIndex < destinationIndex) {
-            this.routePosition = 'right';
-        } else {
-            this.routePosition = 'left';
-        }
-        navigate(destinationPath);
+    if (!sourceRoute) {
+      navigate(destinationPath);
+      return;
     }
+    const sourceIndex = this.getRoutePosition(sourceRoute[0]);
+    const destinationIndex = this.getRoutePosition(destinationRoute[0]);
 
-    private getRoutePosition(routeName: string): number {
-        const routeKeys = Object.keys(routes);
-        const sourceIndex = routeKeys.indexOf(routeName);
-        if (sourceIndex === -1) {
-            throw new Error('Route not found');
-        }
-        return sourceIndex;
+    if (sourceIndex < destinationIndex) {
+      this.routePosition = 'right';
+    } else {
+      this.routePosition = 'left';
     }
+    navigate(destinationPath);
+  }
+
+  private getRoutePosition(routeName: string): number {
+    const routeKeys = Object.keys(routes);
+    const sourceIndex = routeKeys.indexOf(routeName);
+    if (sourceIndex === -1) {
+      throw new Error('Route not found');
+    }
+    return sourceIndex;
+  }
 }
 
 const NavigationContext = createContext(new NavigationStore());
