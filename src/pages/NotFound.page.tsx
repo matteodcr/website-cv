5d96bacd0a5cbfaaa3bd85628f3775a461041656
Matteo Decorsaire <mattdecorsaire@gmail.com>
2024-06-22 20:04:38 -0400

    Fix Navigation.store.tsx

--- src/pages/NotFound.page.tsx
+++ src/pages/NotFound.page.tsx
@@ -1,22 +1,27 @@
-import { Center, Image, Stack, Text, Title } from '@mantine/core';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import {Anchor, Center, Image, Stack, Text, Title} from '@mantine/core';
 import notFoundPage from '@/assets/404.png';
+import {useNavigate} from "react-router-dom";
 
 export default function NotFoundPage() {
+    const navigate = useNavigate();
     return (
         <Stack bg="var(--mantine-color-body)" align="stretch" justify="center" gap="md">
             <Center>
                 <Image
-                  w="300"
-                  src={<LazyLoadImage
+                    w="300"
                     src={notFoundPage} // use normal <img> attributes as props
-                    />} />
+                />
             </Center>
             <Center>
-                <Title>404 - Page Not Found</Title>
+                <Title>404 - Page Non Trouvée</Title>
             </Center>
             <Center>
-                <Text>Sorry, the page you are looking for does not exist.</Text>
+                <Text>Désolé, cette page n'existe pas</Text>
+            </Center>
+            <Center>
+                <Anchor onClick={() => navigate('/')} target="_blank">
+                    Retour à l'accueil
+                </Anchor>
             </Center>
         </Stack>
     );
--- src/store/Navigation.store.tsx
+++ src/store/Navigation.store.tsx
@@ -1,44 +1,47 @@
-import { createContext, useContext } from 'react';
-import { makeAutoObservable } from 'mobx';
-import { NavigateFunction } from 'react-router-dom';
-import { routes } from '@/Router';
+import {createContext, useContext} from 'react';
+import {makeAutoObservable} from 'mobx';
+import {NavigateFunction} from 'react-router-dom';
+import {routes} from '@/Router';
 
 class NavigationStore {
-  routePosition: 'left' | 'right' = 'left';
+    routePosition: 'left' | 'right' = 'left';
 
-  constructor() {
-    makeAutoObservable(this);
-  }
-
-  navigate(navigate: NavigateFunction, sourcePath: string, destinationPath: string) {
-    const sourceRoute = Object.entries(routes).find(([, route]) => route.path === sourcePath);
-    const destinationRoute = Object.entries(routes).find(
-      ([, route]) => route.path === destinationPath
-    );
-
-    if (!sourceRoute || !destinationRoute) {
-      throw new Error('Invalid route path');
+    constructor() {
+        makeAutoObservable(this);
     }
 
-    const sourceIndex = this.getRoutePosition(sourceRoute[0]);
-    const destinationIndex = this.getRoutePosition(destinationRoute[0]);
+    navigate(navigate: NavigateFunction, sourcePath: string, destinationPath: string) {
+        const sourceRoute = Object.entries(routes).find(([, route]) => route.path === sourcePath);
+        const destinationRoute = Object.entries(routes).find(
+            ([, route]) => route.path === destinationPath
+        );
 
-    if (sourceIndex < destinationIndex) {
-      this.routePosition = 'right';
-    } else {
-      this.routePosition = 'left';
-    }
-    navigate(destinationPath);
-  }
+        if (!destinationRoute) {
+            throw new Error(`Invalid route path: ${sourcePath} to ${destinationPath}`);
+        }
+        if (!sourceRoute) {
+            navigate(destinationPath);
+            return;
+        }
+        const sourceIndex = this.getRoutePosition(sourceRoute[0]);
+        const destinationIndex = this.getRoutePosition(destinationRoute[0]);
 
-  private getRoutePosition(routeName: string): number {
-    const routeKeys = Object.keys(routes);
-    const sourceIndex = routeKeys.indexOf(routeName);
-    if (sourceIndex === -1) {
-      throw new Error('Route not found');
+        if (sourceIndex < destinationIndex) {
+            this.routePosition = 'right';
+        } else {
+            this.routePosition = 'left';
+        }
+        navigate(destinationPath);
     }
-    return sourceIndex;
-  }
+
+    private getRoutePosition(routeName: string): number {
+        const routeKeys = Object.keys(routes);
+        const sourceIndex = routeKeys.indexOf(routeName);
+        if (sourceIndex === -1) {
+            throw new Error('Route not found');
+        }
+        return sourceIndex;
+    }
 }
 
 const NavigationContext = createContext(new NavigationStore());
