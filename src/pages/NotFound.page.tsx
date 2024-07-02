f546261d02c39c33dfd24a3a916535c4465496b7
Matteo Decorsaire <mattdecorsaire@gmail.com>
2024-07-01 22:45:53 -0400

    Fix not found

--- src/pages/NotFound.page.tsx
+++ src/pages/NotFound.page.tsx
@@ -1,17 +1,29 @@
-import { Center, Image, Stack, Text, Title } from '@mantine/core';
+import { Anchor, Center, Image, Stack, Text, Title } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+
+import notFoundPage from '@/assets/404.png';
 
 export default function NotFoundPage() {
-  return (
-    <Stack bg="var(--mantine-color-body)" align="stretch" justify="center" gap="md">
-      <Center>
-        <Image w="300" src="src/404.png" />
-      </Center>
-      <Center>
-        <Title>404 - Page Not Found</Title>
-      </Center>
-      <Center>
-        <Text>Sorry, the page you are looking for does not exist.</Text>
-      </Center>
-    </Stack>
-  );
+    const navigate = useNavigate();
+    return (
+        <Stack bg="var(--mantine-color-body)" align="stretch" justify="center" gap="md">
+            <Center>
+                <Image
+                  w="300"
+                  src={notFoundPage} // use normal <img> attributes as props
+                />
+            </Center>
+            <Center>
+                <Title>404 - Page Non Trouvée</Title>
+            </Center>
+            <Center>
+                <Text>Désolé, cette page est inexistante</Text>
+            </Center>
+            <Center>
+                <Anchor onClick={() => navigate('/')} target="_blank">
+                    Retour
+                </Anchor>
+            </Center>
+        </Stack>
+    );
 }
