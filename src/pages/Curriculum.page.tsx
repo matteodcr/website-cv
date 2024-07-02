6fc91d1a43296c723c6e90c1689ee70b6a934910
Matteo Decorsaire <mattdecorsaire@gmail.com>
2024-07-01 22:33:25 -0400

    Fix CV Button

--- src/pages/Curriculum.page.tsx
+++ src/pages/Curriculum.page.tsx
@@ -1,5 +1,6 @@
-import { Stack, Title } from '@mantine/core';
+import { ActionIcon, Group, rem, Stack, Title } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
+import { IconFileTypeDoc } from '@tabler/icons-react';
 import AnimatedPage from '@/components/AnimatedPage';
 import CurriculumCard, { CurriculumProps } from '@/components/CurriculumCard';
 import { useNavigationStore } from '@/store/Navigation.store';
@@ -13,6 +14,8 @@ import polytechBg from '../assets/experience/polytech-bg.jpg';
 import fitBg from '../assets/experience/fit-bg.jpg';
 import etsBg from '../assets/experience/ets-bg.png';
 import mcnBg from '../assets/experience/mcn-bg.jpg';
+
+const URL_CV = 'https://docs.google.com/document/d/1D-tLcVnSyjpBRY2gj7bouo9WOf_YJR1eCTuDnF-UDBg/edit?usp=sharing';
 
 const formationData: CurriculumProps[] = [
   {
@@ -73,7 +76,27 @@ export const CurriculumPage = observer(() => {
 
   return (
     <AnimatedPage direction={store.routePosition}>
-      <Title>Curriculum</Title>
+      <Group> <Title>Curriculum</Title>
+        <ActionIcon
+          size={42}
+          onClick={() =>
+            window.open(
+                URL_CV,
+                '_blank',
+                'noopener noreferrer'
+            )
+        }
+          variant="default"
+          aria-label="ActionIcon with size as a number">
+          <IconFileTypeDoc
+            style={{
+                width: rem(24),
+                height: rem(24),
+              }}
+          />
+        </ActionIcon>
+      </Group>
+
       <CategoryTitle title="Formation" />
       <Stack>
         <Stack gap="xl">
